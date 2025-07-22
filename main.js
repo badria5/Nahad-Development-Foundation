bb65cf13e84fc5811ed6439d123bda3db85ef81e
Badria <badria@gmail.com>
2025-07-23 00:12:12 +0300

    create home page with style

---
+++ main.js
@@ -0,0 +1,9 @@
+function moveImage(image) {
+        // تحريك الصورة عند تمرير المؤشر
+        image.style.transform = 'scale(1.1)'; // تكبير الصورة قليلاً
+    }
+
+    function resetImage(image) {
+        // إعادة الصورة إلى حجمها الأصلي
+        image.style.transform = 'scale(1)'; // إعادة الحجم الأصلي
+    }
