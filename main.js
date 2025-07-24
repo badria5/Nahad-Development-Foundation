1e7e87df1a398197cf7403ae639390358b91bde7
Badria <badria@gmail.com>
2025-07-24 08:40:37 +0300

    update styles

--- main.js
+++ main.js
@@ -7,4 +7,36 @@ function moveImage(image) {
         // إعادة الصورة إلى حجمها الأصلي
         image.style.transform = 'scale(1)'; // إعادة الحجم الأصلي
     }
+/*    const toggleButton = document.getElementById('toggle-button');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    body.classList.toggle('light');
+});*/
+
+const toggleButton = document.getElementById('toggle-button');
+const body = document.body;
+
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark');
+    body.classList.remove('light');
+} else {
+    body.classList.add('light');
+}
+
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    body.classList.toggle('light');
+
+    
+    if (body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
+
+
    
